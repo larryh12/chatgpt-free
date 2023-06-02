cf00666792c8e2b8615f27681c353a4413c55952
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-06-02 17:32:56 +0700

    Add next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,35 @@
-import Sidebar from '@/components/Sidebar';
 import './globals.css';
+import Sidebar from '@/components/Sidebar';
+import { SessionProvider } from '@/components/SessionProvider';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import Login from '@/components/Login';
 
 export const metadata = {
   title: 'ChatGPT Free',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html data-theme="dark" lang="en">
-      <body className="flex">
-        <Sidebar />
-        {/* ClientProvider - Notification */}
-        <div className="flex-1">{children}</div>
+      <body>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <Sidebar />
+              {/* ClientProvider - Notification */}
+              <div className="flex-1">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
