12a47ce45d35b21b7c2cda33ad37d6e9ab87e002
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-06-03 02:16:24 +0700

    Add signOut() to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,13 @@
+'use client';
+
+import { useSession, signOut } from 'next-auth/react';
+import Image from 'next/image';
 import React from 'react';
 import NewChat from './NewChat';
 
 function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="hidden h-screen w-60 flex-col overflow-y-scroll bg-base-300 p-4 md:flex">
       <div className="flex-1">
@@ -11,6 +17,18 @@ function Sidebar() {
           {/* map through the chat row */}
         </div>
       </div>
+
+      {session && (
+        <div onClick={() => signOut()} className="mx-auto cursor-pointer">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={session.user?.image!}
+            alt="User profile picture"
+            className="m-auto h-8 w-8 rounded-full hover:opacity-50"
+          />
+          <p className="link m-auto text-sm">Sign out</p>
+        </div>
+      )}
     </div>
   );
 }
