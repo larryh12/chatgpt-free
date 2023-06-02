97f0bed77906a0e6430d1d4593f1b1c4a4e420e5
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-06-03 02:45:12 +0700

    Add createNewChat()

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,9 +1,33 @@
+'use client';
+
 import React from 'react';
 import { PlusIcon } from '@heroicons/react/24/solid';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { db } from '@/firebase';
 
 function NewChat() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createNewChat = async () => {
+    const doc = await addDoc(
+      collection(db, 'users', session?.user?.email!, 'chats'),
+      {
+        userId: session?.user?.email!,
+        createdAt: serverTimestamp(),
+      }
+    );
+
+    router.push(`/chat/${doc.id}`);
+  };
+
   return (
-    <div className="btn-outline btn-block btn justify-start normal-case">
+    <div
+      onClick={createNewChat}
+      className="btn-outline btn-block btn justify-start normal-case"
+    >
       <PlusIcon className="h-4 w-4" />
       New chat
     </div>
