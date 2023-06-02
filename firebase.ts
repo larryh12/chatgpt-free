58ef754de00d6e28e7a7eee8853b84e90642661f
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-06-03 02:30:17 +0700

    Set up firebase

---
+++ firebase.ts
@@ -0,0 +1,23 @@
+import { getApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAAqoK5qCF37f7SFbLRDln27TOlNQU0myU',
+  authDomain: 'chatgpt-free-larryh21.firebaseapp.com',
+  projectId: 'chatgpt-free-larryh21',
+  storageBucket: 'chatgpt-free-larryh21.appspot.com',
+  messagingSenderId: '285316896679',
+  appId: '1:285316896679:web:21452bb8c43d5e0c908882',
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
