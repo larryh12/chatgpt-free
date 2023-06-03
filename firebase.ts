96b74f2ed7760964bc30aaafa19c8a172c681b0a
Larry Huynh <111525408+larryh12@users.noreply.github.com>
2023-06-03 14:58:47 +0700

    Update config

--- firebase.ts
+++ firebase.ts
@@ -1,12 +1,7 @@
 import { getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyAAqoK5qCF37f7SFbLRDln27TOlNQU0myU',
   authDomain: 'chatgpt-free-larryh21.firebaseapp.com',
